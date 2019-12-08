8f059a667ec27ff1b2f29d37febb6b045c81748a
Karolina <karolina9822@gmail.com>
2019-12-08 19:42:16 +0100

    update clock

--- timer.js
+++ timer.js
@@ -1,17 +1,16 @@
-function odliczanie()
-{
-	var dzisiaj = new Date();
-	
-	var godzina = dzisiaj.getHours();
-	if(godzina<10)godzina ="0"+godzina;
-	
-	var minuta = dzisiaj.getMinutes();
-	if(minuta<10)minuta ="0"+minuta;
-	
-	var sekunda = dzisiaj.getSeconds();
-	if(sekunda<10)sekunda = "0"+sekunda;
-	
-	document.getElementByld("zegar").innerHTML = godzina+":"+minuta+":"+sekunda;
-	
-	setTimeout("odliczanie()",1000);
+function odliczanie() {
+	var data = new Date();
+	var godzina = data.getHours();
+	var minuta = data.getMinutes();
+	var sekunda = data.getSeconds();
+
+
+	if (minuta < 10) minuta = "0" + minuta;
+	if (sekunda < 10) sekunda = "0" + sekunda;
+
+
+	var pokazDate = godzina + ':' + minuta + ':' + sekunda;
+	document.getElementById("zegar").innerHTML = pokazDate;
+
+	setTimeout(odliczanie, 1000);
 }
